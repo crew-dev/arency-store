62a66146d846856a01301941102b5b830921d13f
Vlad Dedikov <69351396+crew-dev@users.noreply.github.com>
2021-02-04 23:22:40 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,1 @@
-window.onload = function () {
-  let preloader = document.getElementById("preloader");
-  preloader.classList.add("hide-preloader");
-  setInterval(function () {
-    preloader.classList.add("preloader-hidden");
-  }, 400);
-};
-
-$(document).ready(function () {
-  setTimeout(function () {
-    $("#preloader").delay(500);
-    $("html, body").css({ overflow: "auto" });
-  }, 500);
-}, 500);
 !function(){var e=window.navigator.userAgent;(e.indexOf("MSIE")>0||e.indexOf("Trident/")>0||e.indexOf("Edge/")>0)&&(location="badbrowser.html")}();const swiper=new Swiper(".swiper-container",{loop:!0,navigation:{nextEl:".slider-button-next",prevEl:".slider-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
